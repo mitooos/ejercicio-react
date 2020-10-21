7a7783a625c89fae7fa4c9cc92b81df54af6e23a
ma.hoyosr <ma.hoyosr@uniandes.edu.co>
2020-10-21 12:15:22 -0500

    se cambia el nombre de un archivo

--- src/components/form.js
+++ src/components/form.js
@@ -1,40 +1,40 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import useForm from "../customHooks/form1";
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import useForm from '../customHooks/form'
 
 const Form = (props) => {
   const { handleChange, handleSubmit, errors } = useForm(
     props.data,
     props.action,
     props.validateFunction
-  );
+  )
   return (
-    <form className="form" onSubmit={handleSubmit}>
+    <form className='form' onSubmit={handleSubmit}>
       {props.data.map((item, i) => {
-        const name = item.name;
+        const name = item.name
         return (
-          <div key={i} className="form-group">
+          <div key={i} className='form-group'>
             <label htmlFor={name}>{name}</label>
             <input
               type={item.type}
               id={name}
               name={name}
-              className="form-control"
+              className='form-control'
               placeholder={item.placeholder}
               onChange={handleChange}
             />
 
             {errors[name] && (
-              <div className="text-danger"> {errors[name]} </div>
+              <div className='text-danger'> {errors[name]} </div>
             )}
           </div>
-        );
+        )
       })}
-      <Button variant="success" type="submit">
+      <Button variant='success' type='submit'>
         Submit
       </Button>
     </form>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
